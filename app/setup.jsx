cd44f9808f8f7cd7f96c972fd451dcc8c46d1c75
Zweer <niccolo@olivieriachille.com>
2017-02-07 15:21:57 +0100

    Setup of the application (store)

--- app/setup.jsx
+++ app/setup.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
 
 import App from './components/app';
+import configureStore from './store/configureStore';
 
-import * as reducers from './reducers';
+export default function setup() {
+  // init relay (?)
+  // init facebook sdk (?)
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
-const reducer = combineReducers(reducers);
-const store = createStoreWithMiddleware(reducer);
+  class Root extends React.Component {
+    constructor() {
+      super();
 
-export default function Root() {
-  return (
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+      this.state = {
+        isLoading: true,
+        store: configureStore(() => this.setState({ isLoading: false })),
+      };
+    }
+
+    render() {
+      if (this.state.isLoading) {
+        return null;
+      }
+
+      return (
+        <Provider store={this.state.store}>
+          <App />
+        </Provider>
+      );
+    }
+  }
+
+  return Root;
 }

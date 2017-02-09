71bb339183f93eb8bded334b452e5cea5863f447
Zweer <niccolo@olivieriachille.com>
2017-02-09 17:30:35 +0100

    Trying to add the background image

--- app/containers/auth/AuthenticateView.js
+++ app/containers/auth/AuthenticateView.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { StyleSheet, TouchableHighlight, Text } from 'react-native';
+import { Image } from 'react-native';
+import { Container, View, Content, Button, Text } from 'native-base';
 import { Actions } from 'react-native-router-flux';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+const background = require('../../images/authenticate.jpg');
 
 export default function Login() {
   return (
-    <TouchableHighlight onPress={Actions.app} style={{ flex: 1 }}>
-      <Text style={styles.container}>
-        Skip Login!!
-      </Text>
-    </TouchableHighlight>
+    <Container style={{ backgroundColor: '#ff0' }}>
+      <View style={{ flex: 1, flexDirection: 'column', backgroundColor: '#f0f' }}>
+        <Content style={{ backgroundColor: '#0ff' }}>
+          <Image source={background} style={{ flex: 1, width: null, height: null, resizeMode: 'cover' }}>
+            <View style={{ flex: 1 }}>
+              <Button onPress={Actions.app} style={{ alignSelf: 'center' }}>
+                <Text>Skip Login</Text>
+              </Button>
+            </View>
+          </Image>
+        </Content>
+      </View>
+    </Container>
   );
 }

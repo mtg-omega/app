786b4148bb98016abd68e592820a33ccd0620ead
Zweer <niccolo@olivieriachille.com>
2017-02-10 11:13:45 +0100

    Fixed the authenticate screen

--- app/containers/auth/AuthenticateView.js
+++ app/containers/auth/AuthenticateView.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { Image } from 'react-native';
-import { Container, View, Content, Button, Text } from 'native-base';
+import { Image, Dimensions } from 'react-native';
+import { Container, View, Button, Text } from 'native-base';
 import { Actions } from 'react-native-router-flux';
 
 const background = require('../../images/authenticate.jpg');
 
+const deviceHeight = Dimensions.get('window').height;
+
 export default function Login() {
   return (
-    <Container style={{ backgroundColor: '#ff0' }}>
-      <View style={{ flex: 1, flexDirection: 'column', backgroundColor: '#f0f' }}>
-        <Content style={{ backgroundColor: '#0ff' }}>
-          <Image source={background} style={{ flex: 1, width: null, height: null, resizeMode: 'cover' }}>
-            <View style={{ flex: 1 }}>
-              <Button onPress={Actions.app} style={{ alignSelf: 'center' }}>
-                <Text>Skip Login</Text>
-              </Button>
-            </View>
-          </Image>
-        </Content>
+    <Container>
+      <View style={{ flex: 1 }}>
+        <Image source={background} style={{ flex: 1, width: null, height: null }}>
+          <View style={{ paddingTop: deviceHeight / 1.75 }}>
+            <Button rounded small onPress={Actions.app} style={{ alignSelf: 'center' }}>
+              <Text>Skip Login</Text>
+            </Button>
+          </View>
+        </Image>
       </View>
     </Container>
   );
